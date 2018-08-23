3ff76fc0f113a9a1c89706dba5c42452862daba8
wangjianhui2464 <wangjianhui2464@gmail.com>
2018-08-23 21:46:41 +0800

    添加可点击交互性

--- lib/main.dart
+++ lib/main.dart
@@ -32,32 +32,34 @@ class RandomWords extends StatefulWidget {
   createState() => new RandomWordsState();
 }
 
-
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
 //    return new Text(wordPair.asPascalCase);
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        if(i.isOdd) return new Divider();
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
 
         final index = i ~/ 2;
-        if(index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -65,7 +67,7 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
 
     return new ListTile(
@@ -74,31 +76,49 @@ class RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
       trailing: new Icon(
-        alreadySaved ? Icons.favorite: Icons.favorite_border,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
-      onTap: (){
-        setState((){
-          if(alreadySaved){
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
             _saved.remove(pair);
-          }else{
+          } else {
             _saved.add(pair);
           }
         });
       },
     );
   }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('收藏列表'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
